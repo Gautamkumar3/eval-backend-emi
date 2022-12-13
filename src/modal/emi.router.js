4d4e8da74dc1aaa5d2750eb60c1bdc44f3ca24b2
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-12-13 23:31:06 +0530

    done

--- src/modal/emi.router.js
+++ src/modal/emi.router.js
@@ -4,7 +4,6 @@ const Authmiddleware = require("../middleware/Authmiddleware");
 
 const app = express.Router();
 
-
 app.post("/calculateEMI", async (req, res) => {
   const { loanAmt, annualInt, tenMonth } = req.body;
   if ((!loanAmt, !annualInt, !tenMonth)) {
@@ -16,7 +15,7 @@ app.post("/calculateEMI", async (req, res) => {
   try {
     let r = annualInt / (12 * 100).toFixed(6);
 
-    let months = tenMonth * 12;
+    let months = tenMonth;
 
     let EMI =
       (loanAmt * r * Math.pow(1 + r, months)) / (Math.pow(1 + r, months) - 1);
