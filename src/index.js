35d843942e19466f06748d210e21c63c3eb812bb
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-12-13 16:57:29 +0530

    kya kar raha bhai

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/db.js");
 const UserRouter = require("./modal/user.router");
-const EmiRouter=require("./modal/emi.router")
+const EmiRouter = require("./modal/emi.router");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", UserRouter);
-app.use("/", EmiRouter)
+app.use("/", EmiRouter);
 
 app.get("/", (req, res) => {
   res.send("Wecome");
